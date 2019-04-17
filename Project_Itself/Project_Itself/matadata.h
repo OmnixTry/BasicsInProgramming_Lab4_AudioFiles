1cf992805b0afa8d6630829ee8c8e3d51e0ec49d
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-04-17 14:03:16 +0300

    metadata classes added

---
+++ Project_Itself/Project_Itself/matadata.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <cstdint>
+#include <string>
+using namespace std;
+
+class riff {
+private:
+	int32_t chunkId;   // Завжди містить значення 0x52494646 (літери "RIFF")
+	int32_t chunkSize; // 36 + розмір другого підрозділу в байтах
+					   // Іншими словами 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
+					   // Це розмір всього файла мінус 8 байтів які займають поля chunkId та chunkSize
+	int32_t format;    // Для wav-файла це завжди 0x57415645 (літери "WAVE")
+public:
+	void fill_riff(string filename);
+
+};
+
+class waw {
+private:
+	int32_t subchunk1Id;   // Завжди 0x666d7420 – літери "fmt "
+	int32_t subchunk1Size; // Завжди 16 для аудіо PCM. Це розмір частини підрозділу, що слідує після цього числа
+	int32_t audioFormat;   // PCM = 1
+	int16_t numChannels;   // Mono = 1, Stereo = 2
+	int32_t sampleRate;    // Наприклад 44100
+	int32_t byteRate;      // == SampleRate * NumChannels * BitsPerSample/8
+	int32_t blockAlign;    // == NumChannels * BitsPerSample/8
+	int16_t bitsPerSample; // 8 bits = 8, 16 bits = 16, etc.
+
+
+
+};
---
+++ Project_Itself/Project_Itself/metadata.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <cstdint>
+#include "matadata.h"
+using namespace std;
+
+void riff::fill_riff(string filename) {
+	ifstream the_file(filename, ios::binary);
+
+
+
+
+}
