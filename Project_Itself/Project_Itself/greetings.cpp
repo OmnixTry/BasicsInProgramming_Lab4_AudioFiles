a377fd50b0cbc5b5674a015886848661fcedff85
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-04-17 13:27:28 +0300

    checking correct input

--- Project_Itself/Project_Itself/greetings.cpp
+++ Project_Itself/Project_Itself/greetings.cpp
@@ -4,11 +4,14 @@
 #include "greetings.h"
 using namespace std;
 
-void check_for_oppen(string filename) {
-	ifstream the_file(filename);
+void check_for_oppen(string *filename) {
+	ifstream the_file(*filename);
 	while (!the_file.is_open()) {
+		the_file.close();
 		cout << "File_name is incorrect!!! No such file!\nInput Again: ";
-		getline(cin, filename);
+		getline(cin, *filename);
+		cin.ignore();
+		the_file.open(*filename);
 	}
-	cout << "file "
+	cout << "File is oppened Succesfuly!!!\n";
 }
--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
-#include "greetings.h"
+//#include "greetings.h"
 using namespace std;
 
-int main(int argc, string argv[]) {
-	
+int main(int argc, char *argv[]) {
+	string input_file = argv[1];
+	string output_file = argv[2];
+	float times = stof(argv[3]);
+	cout << input_file << endl << output_file << endl << times << endl;
+	//check_for_oppen(input_file);
 
 
 
