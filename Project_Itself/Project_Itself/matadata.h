55839b8c9637c45aea3c780d7526f3014088c807
CreatorTheAsh <46164525+CreatorTheAsh@users.noreply.github.com>
2019-05-15 06:46:03 +0300

    Внесены изменения в структуре класса для коректного чтения*(audioFormat и blockAlign должны быть int16_t, а не int32_t), реализовано чтения метаданных и вывод их на экран.

--- Project_Itself/Project_Itself/matadata.h
+++ Project_Itself/Project_Itself/matadata.h
@@ -5,7 +5,8 @@
 #include <string>
 using namespace std;
 
-class riff {
+class riff
+{
 private:
 	int32_t chunkId;   // Завжди містить значення 0x52494646 (літери "RIFF")
 	int32_t chunkSize; // 36 + розмір другого підрозділу в байтах
@@ -14,37 +15,42 @@ private:
 	int32_t format;    // Для wav-файла це завжди 0x57415645 (літери "WAVE")
 public:
 	void fill_riff(string filename);
-
-
+	void show_riff();
 };
 
-class subchank1 {
+class subchank1
+{
 private:
 	int numberOFchunk;
 	int32_t subchunk1Id;   // Завжди 0x666d7420 – літери "fmt "
 	int32_t subchunk1Size; // Завжди 16 для аудіо PCM. Це розмір частини підрозділу, що слідує після цього числа
-	int32_t audioFormat;   // PCM = 1
+	int16_t audioFormat;   // PCM = 1
 	int16_t numChannels;   // Mono = 1, Stereo = 2
 	int32_t sampleRate;    // Наприклад 44100
 	int32_t byteRate;      // == SampleRate * NumChannels * BitsPerSample/8
-	int32_t blockAlign;    // == NumChannels * BitsPerSample/8
+	int16_t blockAlign;    // == NumChannels * BitsPerSample/8
 	int16_t bitsPerSample; // 8 bits = 8, 16 bits = 16, etc.
 public:
-	void fill_subchank (string filename);
+	void fill_subchank1 (string filename);
+	void show_subchank1();
 };
 
 
-class subchank2 {
+class subchank2
+{
 private:
-	int numberOFchunk;
-	int32_t subchunk1Id;   // Завжди 0x666d7420 – літери "fmt "
-	int32_t subchunk1Size; // Завжди 16 для аудіо PCM. Це розмір частини підрозділу, що слідує після цього числа
-	int32_t audioFormat;   // PCM = 1
-	int16_t numChannels;   // Mono = 1, Stereo = 2
-	int32_t sampleRate;    // Наприклад 44100
-	int32_t byteRate;      // == SampleRate * NumChannels * BitsPerSample/8
-	int32_t blockAlign;    // == NumChannels * BitsPerSample/8
-	int16_t bitsPerSample; // 8 bits = 8, 16 bits = 16, etc.
+	int32_t subchunk2Id;   // 0x64617461 – літери "data"
+	int32_t subchunk2Size; // == NumSamples * NumChannels * BitsPerSample/8, кількість байтів аудіоданих
+
+	//int8_t *data = new int8_t[];         // семпли
 public:
-	//void fill_subchank(string filename);
+	void fill_subchank2(string filename);
+	void show_subchank2();
+};
+
+class WAV :public  riff, public subchank1, public subchank2
+{
+public:
+	void fill_wav(string filename);
+	void show_wav();
 };
--- Project_Itself/Project_Itself/metadata.cpp
+++ Project_Itself/Project_Itself/metadata.cpp
@@ -12,19 +12,70 @@ void riff::fill_riff(string filename)
 	in.close();
 }
 
-
-void subchank1::fill_subchank(string filename)
+void riff::show_riff()
 {
-	ifstream in(filename, ios::binary);	subchank1 sub; sub;
-	int trash;
-	in.seekg(12);
+	cout << "chunkId\t\t" << chunkId << "\n";
+	cout << "chunkSize\t" << chunkSize << "\n";
+	cout << "format\t\t" << format << "\n";
+}
+
+void subchank1::fill_subchank1(string filename)
+{
+	ifstream in(filename, ios::binary);
+
 	in.read((char*)&subchunk1Id, sizeof(subchunk1Id));
 	in.read((char*)&subchunk1Size, sizeof(subchunk1Size));
-	//in.read((char*)&audioFormat, sizeof(audioFormat));		2 bytes
-	//in.read((char*)&numChannels, sizeof(numChannels));		2 bytes
+
+	in.read((char*)&audioFormat, sizeof(audioFormat));		//  2 bytes
+	in.read((char*)&numChannels, sizeof(numChannels));		//  2 bytes
+
 	in.read((char*)&sampleRate, sizeof(sampleRate));
 	in.read((char*)&byteRate, sizeof(byteRate));
-	//in.read((char*)&blockAlign, sizeof(blockAlign));			2 bytes
-	//in.read((char*)&bitsPerSample, sizeof(bitsPerSample));	2 bytes
 
+	in.read((char*)&blockAlign, sizeof(blockAlign));		//	2 bytes
+	in.read((char*)&bitsPerSample, sizeof(bitsPerSample));  //  2 bytes
+	in.close();
+}
+
+void subchank1::show_subchank1()
+{
+	cout << "subchunk1Id\t" << subchunk1Id << "\n";
+	cout << "subchunk1Size\t" << subchunk1Size << "\n";
+	cout << "audioFormat\t" << audioFormat << "\n";
+	cout << "numChannels\t" << numChannels << "\n";
+	cout << "sampleRate\t" << sampleRate << "\n";
+	cout << "byteRate\t" << byteRate << "\n";
+	cout << "blockAlign\t" << blockAlign << "\n";
+	cout << "bitsPerSample\t" << bitsPerSample << "\n";
+}
+
+void subchank2::fill_subchank2(string filename)
+{
+	ifstream in(filename, ios::binary);
+	in.seekg(36);
+	in.read((char*)&subchunk2Id, sizeof(subchunk2Id));// по 4 байта
+	in.read((char*)&subchunk2Size, sizeof(subchunk2Size));
+	in.close();
+}
+
+void subchank2::show_subchank2()
+{
+	cout << "subchunk2Id\t"<< subchunk2Id <<"\n";
+	cout << "subchunk2Size\t"<< subchunk2Size <<"\n";
+	//cout << "\t"<<  <<"\n"; for data
+	//cout << "\t"<<  <<"\n";
+}
+
+void WAV::fill_wav(string filename)
+{
+	fill_riff(filename);
+	fill_subchank1(filename);
+	fill_subchank2(filename);
+}
+
+void WAV::show_wav()
+{
+	show_riff();
+	show_subchank1();
+	show_subchank2();
 }
