f0c5dbddfcd1dcd2459784c2c1a7c3f57d6150c5
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-05-25 14:59:32 +0300

    minus extention added

--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -7,18 +7,21 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	/*
 	string input_file = argv[1];
 	string output_file = argv[2];
 	float times = stof(argv[3]);
 	cout << input_file << endl << output_file << endl << times << endl;
 	check_for_oppen(&input_file);
+	*/
+	string input_file = "-.wav";
+	string output_file = "result.wav";
+	float times = -1.5;
 
-	//string input_file = "-.wav";
-	//string output_file = "result.wav";
-	//float times = 1.5;
-
-	wild_nixon_metadata(input_file, output_file, times);
-	linear_interpolation(times, input_file, output_file);
+	wild_nixon_metadata(input_file, output_file, abs(times));
+	wild_nixon_metadata(input_file, "prev.wav", abs(times));
+	if (times > 0) linear_interpolation(times, input_file, output_file);
+	else if (times < 0) linear_interpolation_minus(times, input_file, output_file);
 	//linear_interpolation();
 
 
