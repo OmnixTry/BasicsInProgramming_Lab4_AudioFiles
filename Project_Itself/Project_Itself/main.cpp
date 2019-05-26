1ae568319467cc0220a1418b1f08e20f93583a4a
t.test <t.test@N-KV-KAL13O-043.officekiev.fozzy.lan>
2019-05-26 14:34:22 +0300

    done at the lesson

--- Project_Itself/Project_Itself/main.cpp
+++ Project_Itself/Project_Itself/main.cpp
@@ -7,15 +7,17 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	/*
 	string input_file = argv[1];
 	string output_file = argv[2];
 	float times = stof(argv[3]);
 	cout << input_file << endl << output_file << endl << times << endl;
 	check_for_oppen(&input_file);
+	*/
 
-	//string input_file = "-.wav";
-	//string output_file = "result.wav";
-	//float times = 1.5;
+	string input_file = "-.wav";
+	string output_file = "result.wav";
+	float times = -1;
 
 	wild_nixon_metadata(input_file, output_file, times);
 	linear_interpolation(times, input_file, output_file);
