14e87b567d285114e7ea083bd09babdd1af8deb5
OmnixTry <42980222+OmnixTry@users.noreply.github.com>
2019-05-26 16:11:20 +0300

    Revert "conflict solving"
    This reverts commit 535a268d525efde641896d0bb574516abc113614, reversing
    changes made to 1ae568319467cc0220a1418b1f08e20f93583a4a.

--- Project_Itself/Project_Itself/extention.h
+++ Project_Itself/Project_Itself/extention.h
@@ -5,4 +5,3 @@ using namespace std;
 void simple_expansion(int times, string original_file, string result_file);
 void linear_interpolation(float times, string original_file, string result_file);
 void linear_interpolation();
-void linear_interpolation_minus(float times, string original_file, string result_file);
